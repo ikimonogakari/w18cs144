c26a932d42ef544edfa4e85c7fdaf9ae03554a3e
jameszrx <jameszrx@gmail.com>
2018-02-22 21:31:13 -0800

    skeleton code & config

---
+++ pj4/db.sh
@@ -0,0 +1,37 @@
+
+use BlogServer
+db.Posts.drop()
+db.Users.drop()
+db.createCollection("Posts")
+db.Posts.bulkWrite([
+	{
+		postid: 1,
+		username: "cs144",
+		created: 1518669344517,
+		modified: 1518669344517,
+		title: "Title 1",
+		body: "Hello, world!"
+	},
+	{
+		postid: 2,
+		username: "cs144",
+		created: 1518669658420,
+		modified: 1518669658420,
+		title: "Title 2",
+		body: "I am here."
+	}
+])
+
+db.createCollection("Users")
+db.Users.bulkWrite([
+	{
+		username: "cs144",
+		password: "$2a$10$2DGJ96C77f/WwIwClPwSNuQRqjoSnDFj9GDKjg6X/PePgFdXoE4W6"
+	},
+	{
+		username: "user2",
+		password: "$2a$10$kTaFlLbfY1nnHnjb3ZUP3OhfsfzduLwl2k/gKLXvHew9uX.1blwne"
+	}
+])
+
+exit
