44e274c4a8bc7ff0ce7e4faf936bb0db823a94bf
jameszrx <jameszrx@gmail.com>
2018-02-23 18:30:28 -0800

    part a and part b

--- pj4/blog-server/app.js
+++ pj4/blog-server/app.js
@@ -27,14 +27,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/usernames', users);
 
 // catch 404 and forward to error handler
+/*
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+*/
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -47,9 +49,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// setup mongodb
-
-var dbUrl = 'mongodb://localhost:27017/BlogServer';
+// setup mongodb and connection
+var dbUrl = 'mongodb://localhost:27017';
+var dbName = 'BlogServer';
 db.connect(dbUrl, function(err){
 	if(err){
 		console.log('Something went wrong when connecting to MongoDB');
@@ -59,10 +61,64 @@ db.connect(dbUrl, function(err){
 	}
 });
 
-/*
-app.get('/blog', function(req, res){
+process.env.PORT = 3000;
+console.log('Port number:' + process.env.PORT);
+
+// Read operation
+var findDocuments = function(collName, queryParams, callback){
+	var collection = db.get().db(dbName).collection(collName);
+	console.log(queryParams);
+	collection.find(queryParams).toArray(function(err, docs){
+		if(err){
+			console.log('Error happens in read process.');
+			callback();
+		} else {
+			console.log(docs);
+			callback(docs);
+		}
+	});
+};
+
+app.get('/blog/:username/:postid', function(req, res){
 	var username = req.params.username;
 	var postid = req.params.postid;
-})
-*/
+	var params = {};
+	if(username != null){
+		params['username'] = username;
+	}
+	if(postid != null){
+		params['postid'] = Number(postid);
+	}
+	findDocuments('Posts', params, function(docs){
+		console.log('prepare to render blogpost.ejs');
+		res.render('blogpost', {'posts' : docs});
+	});
+});
+
+app.get('/blog/:username', function(req, res){
+	var username = req.params.username;
+	var offset = req.query.start;
+
+	var params = {};
+	if(username != null){
+		params['username'] = username;
+	}
+	var start = 0;
+	if(offset != null){
+		start = Number(offset);
+	}
+	console.log("offset is:" + offset);
+
+	params['postid'] = {$gt: start, $lt: start+6};
+	findDocuments('Posts', params, function(docs){
+		console.log('prepare to render personBlog.ejs');
+		var nextStartUrl = 'localhost:3000/blog/'+username+'?start='+String(start+5);
+		console.log(nextStartUrl);
+		if(docs.length < 5){
+			nextStartUrl = 'javascript:void(0);';
+		}
+		res.render('bloglist', {'posts' : docs, 'nextUrl': nextStartUrl});
+	});
+});
+
 module.exports = app;
